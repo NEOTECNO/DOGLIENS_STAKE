befa622bec56b741ad3f2b117bb83372ef6b4e85
Keanu Evanz <94647238+NEOTECNO@users.noreply.github.com>
2022-11-08 15:25:48 -0300

    Update 7663-js-sale.js

--- js/7663-js-sale.js
+++ js/7663-js-sale.js
@@ -382,7 +382,7 @@ const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 
 const currentYear = new Date().getUTCFullYear();
-const launchDate = new Date(`Dec 1 ${currentYear} 12:00:00`);
+const launchDate = new Date(`Dec 12 ${currentYear} 16:00:00`);
 
 var diff = null;
 
